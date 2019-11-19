8590273683cdea7963007edca66eda308397e540
“IanMacCode” <ianmicmac@hotmail.com>
2019-11-19 16:12:53 -0500

    Add id's to orders so 'undefined' is not show in the links, fix indentation

--- src/network.js
+++ src/network.js
@@ -96,8 +96,9 @@ export const Network = {
     getOrders: (restaurantId) => {
         return [
             {
-               title: 'order #1',
-               listItems: [
+                id: 0,
+                title: 'order #1',
+                listItems: [
                     {
                         title: 'Carrot',
                         price: 4,
@@ -131,49 +132,51 @@ export const Network = {
                ]
             },
             {
+                id: 1,
                 title: 'order #2',
                 listItems: [
-                        {
-                            title: 'Steak',
-                            price: 20,
-                            img: 'https://www.jessicagavin.com/wp-content/uploads/2019/02/carrots-7-600x900.jpg',
-                            id: 0
-                        },
-                        {
-                            title: 'Carrot',
-                            price: 4,
-                            img: 'https://www.jessicagavin.com/wp-content/uploads/2019/02/carrots-7-600x900.jpg',
-                            id: 0
-                        },
-                        {
-                            title: 'Fish',
-                            price: 15.50,
-                            img: 'https://www.jessicagavin.com/wp-content/uploads/2019/02/carrots-7-600x900.jpg',
-                            id: 0
-                        },
-                        {
-                            title: 'Cake',
-                            price: 15,
-                            img: 'https://www.jessicagavin.com/wp-content/uploads/2019/02/carrots-7-600x900.jpg',
-                            id: 0
-                        },
+                    {
+                        title: 'Steak',
+                        price: 20,
+                        img: 'https://www.jessicagavin.com/wp-content/uploads/2019/02/carrots-7-600x900.jpg',
+                        id: 0
+                    },
+                    {
+                        title: 'Carrot',
+                        price: 4,
+                        img: 'https://www.jessicagavin.com/wp-content/uploads/2019/02/carrots-7-600x900.jpg',
+                        id: 0
+                    },
+                    {
+                        title: 'Fish',
+                        price: 15.50,
+                        img: 'https://www.jessicagavin.com/wp-content/uploads/2019/02/carrots-7-600x900.jpg',
+                        id: 0
+                    },
+                    {
+                        title: 'Cake',
+                        price: 15,
+                        img: 'https://www.jessicagavin.com/wp-content/uploads/2019/02/carrots-7-600x900.jpg',
+                        id: 0
+                    },
                 ]
             },
             {
+                id: 2,
                 title: 'order #3',
                 listItems: [
-                        {
-                            title: 'Steak',
-                            price: 20,
-                            img: 'https://www.jessicagavin.com/wp-content/uploads/2019/02/carrots-7-600x900.jpg',
-                            id: 0
-                        },
-                        {
-                            title: 'Carrot',
-                            price: 4,
-                            img: 'https://www.jessicagavin.com/wp-content/uploads/2019/02/carrots-7-600x900.jpg',
-                            id: 0
-                        },
+                    {
+                        title: 'Steak',
+                        price: 20,
+                        img: 'https://www.jessicagavin.com/wp-content/uploads/2019/02/carrots-7-600x900.jpg',
+                        id: 0
+                    },
+                    {
+                        title: 'Carrot',
+                        price: 4,
+                        img: 'https://www.jessicagavin.com/wp-content/uploads/2019/02/carrots-7-600x900.jpg',
+                        id: 0
+                    },
                 ]
             }
         ]
